b57e5b387597bb08d2d96583d80db40ab2c8006c
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-01-23 16:22:12 +0530

    11-jan-2024

--- jan24/11-jan-2024/maxDiffBtwodeAndAncestor.java
+++ jan24/11-jan-2024/maxDiffBtwodeAndAncestor.java
@@ -1,8 +1,11 @@
+import javax.swing.tree.TreeNode;
+
 package 11-jan-2024;
 
 public class maxDiffBtwodeAndAncestor {
     /*
      * Name Of The Problem :- 1026. Maximum Difference Between Node and Ancestor
+     * Date :- 11-jan-2024
      * Given the root of a binary tree, find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
 
 A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
