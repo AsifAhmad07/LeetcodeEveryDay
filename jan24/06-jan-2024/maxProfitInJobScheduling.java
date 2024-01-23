1094ec28b355f485c7543a05058e9a60c058dc82
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-01-23 16:15:45 +0530

    06-jan-2024

--- jan24/06-jan-2024/maxProfitInJobScheduling.java
+++ jan24/06-jan-2024/maxProfitInJobScheduling.java
@@ -3,6 +3,7 @@ package 06-jan-2024;
 public class maxProfitInJobScheduling {
     /*
      * Name Of The Problem :- 1235. Maximum Profit in Job Scheduling
+     * Date :- 06-jan-2024
      * We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
 
 You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
