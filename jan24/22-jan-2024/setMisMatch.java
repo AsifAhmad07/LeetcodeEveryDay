256ca6eecab8087b1250fd480dd87aaec1ff77bb
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-01-23 17:29:46 +0530

    22-jan-2024

--- jan24/22-jan-2024/setMisMatch.java
+++ jan24/22-jan-2024/setMisMatch.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 package 22-jan-2024;
 
 public class setMisMatch {
 
     /*
      * Name Of The Problem :- 645. Set Mismatch
+     * Date :- Date :- 22-jan-2024
      * You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 
 You are given an integer array nums representing the data status of this set after the error.
