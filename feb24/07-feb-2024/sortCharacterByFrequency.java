43441a760747178ddb1aa18c66be01280a204bd8
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-02-07 14:54:05 +0530

    07-feb-2024

---
+++ feb24/07-feb-2024/sortCharacterByFrequency.java
@@ -0,0 +1,77 @@
+import javax.xml.crypto.Data;
+
+package 07-feb-2024;
+
+public class sortCharacterByFrequency {
+
+
+    Name Of The Problem :- 451. Sort Characters By Frequency
+
+
+    Date :- 07-feb-2024
+
+    Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 105
+s consists of uppercase and lowercase English letters and digits.
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+    public String frequencySort(String s) {
+
+           Map<Character, Integer> freqMap = new HashMap<>();
+        
+        for (char ch : s.toCharArray()) {
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        }
+        
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(freqMap.entrySet());
+        
+        list.sort((a, b) -> b.getValue() - a.getValue());
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (Map.Entry<Character, Integer> entry : list) {
+            char ch = entry.getKey();
+            int freq = entry.getValue();
+            for (int i = 0; i < freq; i++) {
+                result.append(ch);
+            }
+        }
+        
+        return result.toString();
+        
+    }
+}
+
+    
+}
