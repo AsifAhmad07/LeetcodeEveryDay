b41136c53ee2cf7e891286f141bfa149064e4fb7
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-04-01 12:09:09 +0530

    01-April-2024

---
+++ april24/01-april-2024/LengthofLastWord.java
@@ -0,0 +1,66 @@
+package 01-april-2024;
+
+public class LengthofLastWord {
+
+
+
+    Name Of The Problem :- 58. Length of Last Word
+
+    Date :- 01-April-2024
+
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        if (n == 0)
+            return 0;
+        int i = n - 1;
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
+
+        int length = 0;
+        for (; i >= 0; i--) {
+            if (s.charAt(i) == ' ')
+                break;
+            length++;
+        }
+        return length;
+        
+    }
+}
+    
+}
