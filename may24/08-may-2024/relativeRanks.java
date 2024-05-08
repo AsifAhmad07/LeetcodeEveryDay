544385a7fdeedfa957d1c2a4c970b50f30c94ae9
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-05-08 19:56:40 +0530

    08-May-2024

---
+++ may24/08-may-2024/relativeRanks.java
@@ -0,0 +1,75 @@
+package 08-may-2024;
+
+public class relativeRanks {
+
+
+    Name Of The Problem :- 506. Relative Ranks
+
+    Date  :- 08-May-2024
+
+    You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+The 1st place athlete's rank is "Gold Medal".
+The 2nd place athlete's rank is "Silver Medal".
+The 3rd place athlete's rank is "Bronze Medal".
+For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+ 
+
+Example 1:
+
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+Example 2:
+
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+ 
+
+Constraints:
+
+n == score.length
+1 <= n <= 104
+0 <= score[i] <= 106
+All the values in score are unique.
+
+*****************************************************************************************************************************************************
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        
+        String[] result = new String[n];
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            mp.put(score[i], i); // ith athlete
+        }
+        
+        Arrays.sort(score); // ascending
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int ath = mp.get(score[i]);
+            if (i == n - 1) {
+                result[ath] = "Gold Medal";
+            } else if (i == n - 2) {
+                result[ath] = "Silver Medal";
+            } else if (i == n - 3) {
+                result[ath] = "Bronze Medal";
+            } else {
+                result[ath] = String.valueOf(n - i);
+            }
+        }
+        
+        return result;
+        
+    }
+}
+    
+}
