f3c29c8935b4aa53e771d296ca76c10f4d34626f
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-05-13 16:34:09 +0530

    13-May-2024

---
+++ may24/13-may-2024/scoreAfterFlippingMatrix.java
@@ -0,0 +1,72 @@
+import java.util.jar.Attributes.Name;
+
+package 13-may-2024;
+
+public class scoreAfterFlippingMatrix {
+
+
+    Name Of The Problem :- 861. Score After Flipping Matrix
+
+    Date :- 13-May-2024
+
+    You are given an m x n binary matrix grid.
+
+A move consists of choosing any row or column and toggling each value in that row or column (i.e., changing all 0's to 1's, and all 1's to 0's).
+
+Every row of the matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
+
+Return the highest possible score after making any number of moves (including zero moves).
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+Output: 39
+Explanation: 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+Example 2:
+
+Input: grid = [[0]]
+Output: 1
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 20
+grid[i][j] is either 0 or 1.
+
+
+*****************************************************************************************************************************************************either
+class Solution {
+    public int matrixScore(int[][] grid) {
+         int m = grid.length;
+        int n = grid[0].length;
+
+        // MSB -> 2^(n-1)
+        int score = (int) Math.pow(2, n - 1) * m;
+
+        for (int j = 1; j < n; j++) {
+            int countSameBits = 0; // count of 1s
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == grid[i][0]) {
+                    countSameBits++;
+                }
+            }
+
+            int countOnes = countSameBits;
+            int countZeros = m - countOnes;
+
+            int ones = Math.max(countOnes, countZeros);
+
+            score += (int) Math.pow(2, n - j - 1) * ones;
+        }
+
+        return score;
+        
+    }
+}
+    
+}
