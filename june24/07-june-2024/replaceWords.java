591243337588ae50e9f4f0732c27aefdd8272366
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-06-07 19:09:02 +0530

    06-June-2024

---
+++ june24/07-june-2024/replaceWords.java
@@ -0,0 +1,71 @@
+package 07-june-2024;
+
+public class replaceWords {
+
+
+    Name Of The Problem :- 648. Replace Words
+
+    Date :- 07-June-2024
+
+    In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word derivative. For example, when the root "help" is followed by the word "ful", we can form a derivative "helpful".
+
+Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the derivatives in the sentence with the root forming it. If a derivative can be replaced by more than one root, replace it with the root that has the shortest length.
+
+Return the sentence after the replacement.
+
+ 
+
+Example 1:
+
+Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+Example 2:
+
+Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+Output: "a a b c"
+ 
+
+Constraints:
+
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 100
+dictionary[i] consists of only lower-case letters.
+1 <= sentence.length <= 106
+sentence consists of only lower-case letters and spaces.
+The number of words in sentence is in the range [1, 1000]
+The length of each word in sentence is in the range [1, 1000]
+Every two consecutive words in sentence will be separated by exactly one space.
+sentence does not have leading or trailing spaces.
+
+*****************************************************************************************************************************************************
+
+
+class Solution {
+     private String findRoot(String word, Set<String> st) {
+        // Try all length substring starting from 0th index
+        for (int l = 1; l <= word.length(); l++) {
+            String root = word.substring(0, l);
+            if (st.contains(root)) {
+                return root;
+            }
+        }
+        return word;
+    }
+    public String replaceWords(List<String> dictionary, String sentence) {
+          Set<String> st = new HashSet<>(dictionary);
+        
+        StringBuilder result = new StringBuilder();
+        String[] words = sentence.split(" ");
+        
+        for (String word : words) {
+            result.append(findRoot(word, st)).append(" ");
+        }
+        
+        // Remove the trailing space
+        return result.toString().trim();
+        
+        
+    }
+}
+    
+}
