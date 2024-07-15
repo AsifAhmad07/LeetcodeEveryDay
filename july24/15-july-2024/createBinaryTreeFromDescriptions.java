f74c4b3974cef540de336ba247664c28fa24c97c
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-07-15 15:08:54 +0530

    15-July-2024

---
+++ july24/15-july-2024/createBinaryTreeFromDescriptions.java
@@ -0,0 +1,93 @@
+package 15-july-2024;
+
+public class createBinaryTreeFromDescriptions {
+
+
+    Name Of The Problem :- 15-July-2024
+
+    You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+If isLefti == 1, then childi is the left child of parenti.
+If isLefti == 0, then childi is the right child of parenti.
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid.
+
+ 
+
+Example 1:
+
+
+Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+Output: [50,20,80,15,17,19]
+Explanation: The root node is the node with value 50 since it has no parent.
+The resulting binary tree is shown in the diagram.
+Example 2:
+
+
+Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
+Output: [1,2,null,null,3,4]
+Explanation: The root node is the node with value 1 since it has no parent.
+The resulting binary tree is shown in the diagram.
+ 
+
+Constraints:
+
+1 <= descriptions.length <= 104
+descriptions[i].length == 3
+1 <= parenti, childi <= 105
+0 <= isLefti <= 1
+The binary tree described by descriptions is valid.
+
+*****************************************************************************************************************************************************
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+         Map<Integer, TreeNode> mp = new HashMap<>();
+        Set<Integer> childSet = new HashSet<>();
+
+        // Create nodes and set up parent-child relationships
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            int child = description[1];
+            boolean isLeft = description[2] == 1;
+
+            mp.putIfAbsent(parent, new TreeNode(parent));
+            mp.putIfAbsent(child, new TreeNode(child));
+
+            if (isLeft) {
+                mp.get(parent).left = mp.get(child);
+            } else {
+                mp.get(parent).right = mp.get(child);
+            }
+
+            childSet.add(child);
+        }
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            if (!childSet.contains(parent)) {
+                return mp.get(parent);
+            }
+        }
+
+        return null;
+        
+    }
+}
+    
+}
