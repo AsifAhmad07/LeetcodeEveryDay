50efb3f4c83dfd2cdcd4f2bcc89fabf8476768b8
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-07-31 20:23:07 +0530

    31-July-2024

---
+++ july24/31-july-2024/fillingBookcaseShelves.java
@@ -0,0 +1,90 @@
+package 31-july-2024;
+
+public class fillingBookcaseShelves {
+
+
+    Name Of The Problem :- 1105. Filling Bookcase Shelves
+
+    Date :- 31-July-2024
+
+    You are given an array books where books[i] = [thicknessi, heighti] indicates the thickness and height of the ith book. You are also given an integer shelfWidth.
+
+We want to place these books in order onto bookcase shelves that have a total width shelfWidth.
+
+We choose some of the books to place on this shelf such that the sum of their thickness is less than or equal to shelfWidth, then build another level of the shelf of the bookcase so that the total height of the bookcase has increased by the maximum height of the books we just put down. We repeat this process until there are no more books to place.
+
+Note that at each step of the above process, the order of the books we place is the same order as the given sequence of books.
+
+For example, if we have an ordered list of 5 books, we might place the first and second book onto the first shelf, the third book on the second shelf, and the fourth and fifth book on the last shelf.
+Return the minimum possible height that the total bookshelf can be after placing shelves in this manner.
+
+ 
+
+Example 1:
+
+
+Input: books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4
+Output: 6
+Explanation:
+The sum of the heights of the 3 shelves is 1 + 3 + 2 = 6.
+Notice that book number 2 does not have to be on the first shelf.
+Example 2:
+
+Input: books = [[1,3],[2,4],[3,2]], shelfWidth = 6
+Output: 4
+ 
+
+Constraints:
+
+1 <= books.length <= 1000
+1 <= thicknessi <= shelfWidth <= 1000
+1 <= heighti <= 1000
+
+
+*****************************************************************************************************************************************************
+
+class Solution {
+    private int n;
+   private int WIDTH;
+   private int[][] t;
+
+   private int solve(int[][] books, int i, int remainW, int maxHt) {
+       if (i >= n) {
+           return maxHt;
+       }
+
+       if (t[i][remainW] != -1) {
+           return t[i][remainW];
+       }
+
+       int bookW = books[i][0];
+       int bookH = books[i][1];
+
+       int keep = Integer.MAX_VALUE;
+       int skip = Integer.MAX_VALUE;
+
+       // keep
+       if (bookW <= remainW) { // let's keep it here
+           keep = solve(books, i + 1, remainW - bookW, Math.max(maxHt, bookH));
+       }
+
+       // skip and put in next shelf
+       skip = maxHt + solve(books, i + 1, WIDTH - bookW, bookH);
+
+       return t[i][remainW] = Math.min(keep, skip);
+   }
+   public int minHeightShelves(int[][] books, int shelfWidth) {
+        n = books.length;
+       WIDTH = shelfWidth;
+       t = new int[1001][1001];
+       for (int[] row : t) {
+           Arrays.fill(row, -1);
+       }
+
+       int remainW = shelfWidth;
+       return solve(books, 0, remainW, 0);
+       
+   }
+}
+    
+}
