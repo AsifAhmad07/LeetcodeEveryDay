b3aa91a4e8281c4d7156b4a556552111fa2c799d
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-08-29 16:41:55 +0530

    29-Aug-2024

---
+++ aug24/29-aug-2024/mostStonesRemovedwithSameRoworColumn.java
@@ -0,0 +1,118 @@
+package 29-aug-2024;
+
+public class mostStonesRemovedwithSameRoworColumn {
+
+
+    Name Of The Problem :- 947. Most Stones Removed with Same Row or Column
+
+
+    Date :- 29-August-2024
+
+
+    On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
+
+A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
+
+Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
+
+ 
+
+Example 1:
+
+Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+Output: 5
+Explanation: One way to remove 5 stones is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,1].
+2. Remove stone [2,1] because it shares the same column as [0,1].
+3. Remove stone [1,2] because it shares the same row as [1,0].
+4. Remove stone [1,0] because it shares the same column as [0,0].
+5. Remove stone [0,1] because it shares the same row as [0,0].
+Stone [0,0] cannot be removed since it does not share a row/column with another stone still on the plane.
+Example 2:
+
+Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+Output: 3
+Explanation: One way to make 3 moves is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,0].
+2. Remove stone [2,0] because it shares the same column as [0,0].
+3. Remove stone [0,2] because it shares the same row as [0,0].
+Stones [0,0] and [1,1] cannot be removed since they do not share a row/column with another stone still on the plane.
+Example 3:
+
+Input: stones = [[0,0]]
+Output: 0
+Explanation: [0,0] is the only stone on the plane, so you cannot remove it.
+ 
+
+Constraints:
+
+1 <= stones.length <= 1000
+0 <= xi, yi <= 104
+No two stones are at the same coordinate point.
+
+
+*****************************************************************************************************************************************************
+class Solution {
+    private int[] parent;
+    private int[] rank;
+    private int n;
+
+    // Find function with path compression
+    private int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]); // Path compression
+        }
+        return parent[i];
+    }
+
+    // Union function with union by rank
+    private void union(int i, int j) {
+        int rootI = find(i);
+        int rootJ = find(j);
+        
+        if (rootI != rootJ) {
+            if (rank[rootI] > rank[rootJ]) {
+                parent[rootJ] = rootI;
+            } else if (rank[rootI] < rank[rootJ]) {
+                parent[rootI] = rootJ;
+            } else {
+                parent[rootJ] = rootI;
+                rank[rootI]++;
+            }
+        }
+    }
+
+    public int removeStones(int[][] stones) {
+        n = stones.length;
+        parent = new int[n];
+        rank = new int[n];
+
+        // Initialize parent and rank arrays
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        // Union stones that are in the same row or column
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    union(i, j);
+                }
+            }
+        }
+
+        // Count the number of disjoint sets (connected components)
+        int groups = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                groups++;
+            }
+        }
+
+        // The number of stones that can be removed is total stones - number of groups
+        return n - groups;
+    }
+}
+    
+}
