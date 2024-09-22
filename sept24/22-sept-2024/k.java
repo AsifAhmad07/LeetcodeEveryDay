5d9c0e980af2302cb0a5bab880b8423e2556988b
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-09-22 12:50:02 +0530

    22-Sept-2024

---
+++ sept24/22-sept-2024/k.java
@@ -0,0 +1,63 @@
+package 22-sept-2024;
+
+public class k-thSmallestinLexicographicalOrder {
+
+
+    Name Of The Problem :- 440. K-th Smallest in Lexicographical Order
+
+
+ Date :- 22-September-2024
+
+ Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+
+ 
+
+Example 1:
+
+Input: n = 13, k = 2
+Output: 10
+Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
+Example 2:
+
+Input: n = 1, k = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= k <= n <= 109
+
+*****************************************************************************************************************************************************
+class Solution {
+    public int countNumbers(long curr, long next, int n) {
+        int countNum = 0;
+
+        while (curr <= n) {
+            countNum += Math.min(next, (long) n + 1) - curr;
+            curr *= 10;
+            next *= 10;
+        }
+
+        return countNum;
+    }
+
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k -= 1; 
+
+        while (k > 0) {
+            int count = countNumbers(curr, curr + 1, n);
+            if (count <= k) {
+                curr++;
+                k -= count; 
+            } else {
+                curr *= 10;
+                k -= 1;
+            }
+        }
+
+        return curr;
+    }
+}
+    
+}
