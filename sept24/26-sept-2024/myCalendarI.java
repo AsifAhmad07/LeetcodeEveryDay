1e47a2e49d2f5d91f8867e6ece70f01478ef1415
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-09-26 16:42:15 +0530

    26-Sept-2024

---
+++ sept24/26-sept-2024/myCalendarI.java
@@ -0,0 +1,73 @@
+package 26-sept-2024;
+
+public class myCalendarI {
+
+
+    Name Of The Problem :- 729. My Calendar I
+
+    Date :- 26-September-2024
+
+    You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendar class:
+
+MyCalendar() Initializes the calendar object.
+boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+ 
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+ 
+
+Constraints:
+
+0 <= start < end <= 109
+At most 1000 calls will be made to book.
+
+
+*****************************************************************************************************************************************************
+
+class MyCalendar {
+    TreeSet<int[]> st;
+
+    public MyCalendar() {
+        st = new TreeSet<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+    }
+
+    public boolean book(int start, int end) {
+        int[] event = new int[]{start, end};
+        int[] next = st.ceiling(event); 
+        if (next != null && next[0] < end) {
+            return false;
+        }
+        int[] prev = st.floor(event);
+        if (prev != null && start < prev[1]) {
+            return false;
+        }
+        st.add(new int[]{start, end});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
+    
+}
