e4b694a3a9b11be1bdcf585af8903ec218743909
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-12-07 20:57:38 +0530

    07-Dec-2024

---
+++ dec24/07-dec-2024/minimumLimitofBallsinaBag.java
@@ -0,0 +1,89 @@
+package 07-dec-2024;
+
+public class minimumLimitofBallsinaBag {
+
+
+    Name Of The Problem :- 1760. Minimum Limit of Balls in a Bag
+
+    Date :- 07-December-2024
+
+    You are given an integer array nums where the ith bag contains nums[i] balls. You are also given an integer maxOperations.
+
+You can perform the following operation at most maxOperations times:
+
+Take any bag of balls and divide it into two new bags with a positive number of balls.
+For example, a bag of 5 balls can become two new bags of 1 and 4 balls, or two new bags of 2 and 3 balls.
+Your penalty is the maximum number of balls in a bag. You want to minimize your penalty after the operations.
+
+Return the minimum possible penalty after performing the operations.
+
+ 
+
+Example 1:
+
+Input: nums = [9], maxOperations = 2
+Output: 3
+Explanation: 
+- Divide the bag with 9 balls into two bags of sizes 6 and 3. [9] -> [6,3].
+- Divide the bag with 6 balls into two bags of sizes 3 and 3. [6,3] -> [3,3,3].
+The bag with the most number of balls has 3 balls, so your penalty is 3 and you should return 3.
+Example 2:
+
+Input: nums = [2,4,8,2], maxOperations = 4
+Output: 2
+Explanation:
+- Divide the bag with 8 balls into two bags of sizes 4 and 4. [2,4,8,2] -> [2,4,4,4,2].
+- Divide the bag with 4 balls into two bags of sizes 2 and 2. [2,4,4,4,2] -> [2,2,2,4,4,2].
+- Divide the bag with 4 balls into two bags of sizes 2 and 2. [2,2,2,4,4,2] -> [2,2,2,2,2,4,2].
+- Divide the bag with 4 balls into two bags of sizes 2 and 2. [2,2,2,2,2,4,2] -> [2,2,2,2,2,2,2,2].
+The bag with the most number of balls has 2 balls, so your penalty is 2, and you should return 2.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= maxOperations, nums[i] <= 109
+
+
+*****************************************************************************************************************************************************
+
+class Solution {
+    public boolean isPossible(int[] nums, int maxOps, int mid) {
+        int totalOps = 0; // To bring each number <= mid
+
+        for (int num : nums) {
+            int ops = num / mid;
+
+            if (num % mid == 0) {
+                ops -= 1;
+            }
+
+            totalOps += ops;
+        }
+
+        return totalOps <= maxOps;
+    }
+
+    public int minimumSize(int[] nums, int maxOperations) {
+        int l = 1;
+        int r = Arrays.stream(nums).max().getAsInt(); // Find max in nums
+        
+        int result = r; // We have to minimize this
+
+        // Binary search: O(n * log(MAX))
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (isPossible(nums, maxOperations, mid)) {
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
+    
+}
