6f4d941c9eb5d5d9eb3d5db8c770babb23ebbcd9
AsifAhmad07 <asifahmadkhan2016@gmail.com>
2024-12-10 13:26:30 +0530

    10-Dec-2024

---
+++ dec24/10-dec-2024/findLongestSpecialSubstringThatOccursThriceI.java
@@ -0,0 +1,79 @@
+package 10-dec-2024;
+
+public class findLongestSpecialSubstringThatOccursThriceI {
+
+
+    Name Of The Problem :- 2981. Find Longest Special Substring That Occurs Thrice I
+
+    Date :- 10-December-2024
+
+    You are given a string s that consists of lowercase English letters.
+
+A string is called special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
+
+Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring occurs at least thrice.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: s = "aaaa"
+Output: 2
+Explanation: The longest special substring which occurs thrice is "aa": substrings "aaaa", "aaaa", and "aaaa".
+It can be shown that the maximum length achievable is 2.
+Example 2:
+
+Input: s = "abcdef"
+Output: -1
+Explanation: There exists no special substring which occurs at least thrice. Hence return -1.
+Example 3:
+
+Input: s = "abcaba"
+Output: 1
+Explanation: The longest special substring which occurs thrice is "a": substrings "abcaba", "abcaba", and "abcaba".
+It can be shown that the maximum length achievable is 1.
+ 
+
+Constraints:
+
+3 <= s.length <= 50
+s consists of only lowercase English letters.
+
+
+*****************************************************************************************************************************************************
+
+class Solution {
+    public int maximumLength(String s) {
+
+         int n = s.length();
+        Map<String, Integer> mp = new HashMap<>(); // Map to store substring and its frequency
+        
+        for (int i = 0; i < n; i++) {
+            StringBuilder curr = new StringBuilder();
+            for (int j = i; j < n; j++) {
+                if (curr.length() == 0 || curr.charAt(curr.length() - 1) == s.charAt(j)) {
+                    curr.append(s.charAt(j));
+                    mp.put(curr.toString(), mp.getOrDefault(curr.toString(), 0) + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        int result = 0;
+        for (Map.Entry<String, Integer> entry : mp.entrySet()) {
+            String str = entry.getKey();
+            int count = entry.getValue();
+            if (count >= 3 && str.length() > result) {
+                result = str.length();
+            }
+        }
+        
+        return result == 0 ? -1 : result;
+        
+    }
+}
+    
+}
